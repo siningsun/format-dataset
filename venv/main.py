56d5757b218c113629a22844582caf75b483b18d
siningsun <690058381@qq.com>
2024-08-03 14:18:15 +0800

    3rd try.

--- venv/main.py
+++ venv/main.py
@@ -4,6 +4,9 @@ with open('./json_files/test.json') as data_file:
     raw_data = json.load(data_file)
 result_list = []
 error_list = []
+yes_list = []
+no_list = []
+
 for data_obj in raw_data:
     input_id = data_obj.get('id')
     input_str = data_obj.get('input').lower()
@@ -27,6 +30,10 @@ for data_obj in raw_data:
         question = core_content[0]
         if len(core_content) > 1:
             answer = core_content[1]
+            if answer == 'yes':
+                yes_list.append(headline_id)
+            else:
+                no_list.append(headline_id)
         else:
             answer = ''
         # construct data object
@@ -38,7 +45,9 @@ for data_obj in raw_data:
         result_list.append(processed_data)
         index += 1
 
-print(len(result_list))
-print(len(error_list))
-json.dump(result_list, open('./json_files/test_result.json', 'w'), indent=4)
-json.dump(error_list, open('./json_files/error_result.json', 'w'), indent=4)
+# print(len(result_list))
+# print(len(error_list))
+print(len(yes_list))
+print(len(no_list))
+json.dump(result_list, open('json_files/result/test_result.json', 'w'), indent=4)
+json.dump(error_list, open('json_files/result/error_result.json', 'w'), indent=4)
